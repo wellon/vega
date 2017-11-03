2a5ad18d31e639a43dbe78ed134edb204a35417f
w3ll0n <wellon676@gmail.com>
2017-11-03 20:10:23 +0300

    Add cascase drop-down list

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -1,9 +1,10 @@
-import { MakeService } from './services/make.service';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
+import { MakeService } from './services/make.service';
+import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,3 +1,4 @@
+import { MakeService } from './../../services/make.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
+  makes: any[];
+  models: any[];
+  vehicle: any = {};
 
-  constructor() { }
+  constructor(private makeService: MakeService) { }
 
   ngOnInit() {
+    this.makeService.getMakes().subscribe(makes =>
+      this.makes = makes);
   }
 
+  onMakeChange(){
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.make)
+    this.models = selectedMake ? selectedMake.models : [];
+  }
 }
