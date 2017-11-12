79330d0a3e1cf1a5144c0d7691492a70aff2489d
w3ll0n <wellon676@gmail.com>
2017-11-12 21:56:49 +0300

    Apply client-side security and remove [required] ann from email in contactResource

--- Controllers/Resources/ContactResource.cs
+++ Controllers/Resources/ContactResource.cs
@@ -7,7 +7,7 @@ namespace vega.Controllers.Resources
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        [Required]
+        
         [StringLength(255)]
         public string Email { get; set; }
         [Required]
