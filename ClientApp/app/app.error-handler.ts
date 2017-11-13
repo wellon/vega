dfaa5182fb80339a94554510d5cbed3ef957faa3
w3ll0n <wellon676@gmail.com>
2017-11-13 22:17:18 +0300

    Capture exception only in production

--- ClientApp/app/app.error-handler.ts
+++ ClientApp/app/app.error-handler.ts
@@ -1,6 +1,6 @@
 import * as Raven from 'raven-js';
 import { ToastyService } from 'ng2-toasty';
-import { ErrorHandler, Inject, NgZone } from '@angular/core';
+import { ErrorHandler, Inject, NgZone, isDevMode } from '@angular/core';
 
 export class AppErrorHandler implements ErrorHandler{
     constructor(
@@ -8,7 +8,10 @@ export class AppErrorHandler implements ErrorHandler{
         @Inject(ToastyService) private toastyService: ToastyService){}
 
     handleError(error: any): void {
-        Raven.captureException(error.originalError || error);
+        if (!isDevMode)
+            Raven.captureException(error.originalError || error);
+        else
+            throw error;
 
         this.ngZone.run(() => {
             this.toastyService.error({
