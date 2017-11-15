d598d94f0cc9c448bf406e2462458482965032ff
w3ll0n <wellon676@gmail.com>
2017-11-15 17:11:14 +0300

    populate the Form

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -52,8 +52,10 @@ export class VehicleFormComponent implements OnInit {
     Observable.forkJoin(sources).subscribe(data => {
       this.makes = data[0];
       this.features = data[1];
-      if (this.vehicle.id)
+      if (this.vehicle.id){
         this.setVehicle(data[2]);
+        this.populateModels();
+      }
     }, err => {
       if (err.status == 404)
         this.router.navigate(['/home']);
@@ -70,9 +72,13 @@ export class VehicleFormComponent implements OnInit {
   }
 
   onMakeChange(){
+    this.populateModels();
+    delete this.vehicle.modelId;
+  }
+
+  private populateModels(){
     var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId)
     this.models = selectedMake ? selectedMake.models : [];
-    delete this.vehicle.modelId;
   }
 
   onFeatureToggle(featureId: any, $event:any){
