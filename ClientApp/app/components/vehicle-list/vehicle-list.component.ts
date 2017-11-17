df40e607946e25634970e018a40984f3552585b5
w3ll0n <wellon676@gmail.com>
2017-11-18 02:24:19 +0300

    pass sort params to the server

--- ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];  
   makes: KeyValuePair[];
-  filter: any = {};
+  query: any = {};
 
   constructor(private vehicleService: VehicleService) { }
 
@@ -22,7 +22,7 @@ export class VehicleListComponent implements OnInit {
   }
 
   private populateVehicles(){
-    this.vehicleService.getVehicles(this.filter)
+    this.vehicleService.getVehicles(this.query)
       .subscribe(vehicles => this.vehicles = vehicles);
   }
 
@@ -31,7 +31,17 @@ export class VehicleListComponent implements OnInit {
   }
 
   resetFilter(){
-    this.filter = {};
+    this.query = {};
     this.onFilterChange();
+  }
+
+  sortBy(columnName: any){
+    if(this.query.sortBy == columnName){
+      this.query.isSortAscending = !this.query.isSortAscending;
+    } else {
+      this.query.sortBy = columnName;
+      this.query.isSortAscending = true;
+    }
+    this.populateVehicles();
   }
 }
