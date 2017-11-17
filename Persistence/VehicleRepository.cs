b8274ea1a9aa5ec191716064704dd67294107f68
w3ll0n <wellon676@gmail.com>
2017-11-18 00:37:53 +0300

    refactoring with dict

--- Persistence/VehicleRepository.cs
+++ Persistence/VehicleRepository.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using vega.Core.Models;
 using vega.Core;
 using System.Linq;
+using System.Linq.Expressions;
+using System;
 
 namespace vega.Persistence
 {
@@ -53,17 +55,18 @@ namespace vega.Persistence
             if(queryObj.ModelId.HasValue)
                 query = query.Where(v => v.Model.Id == queryObj.ModelId);
 
-            if(queryObj.SortBy == "make")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Make.Name) : query.OrderByDescending(v => v.Model.Make.Name);
-            
-            if(queryObj.SortBy == "model")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Model.Name) : query.OrderByDescending(v => v.Model.Name);
-            
-            if(queryObj.SortBy == "contact")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.ContactName) : query.OrderByDescending(v => v.ContactName);
-            
-            if(queryObj.SortBy == "id")
-                query = (queryObj.IsSortAscending) ? query.OrderBy(v => v.Id) : query.OrderByDescending(v => v.Id);
+            var columnsMap = new Dictionary<string, Expression<Func<Vehicle, object>>>()
+            {
+                ["make"] = v => v.Model.Make.Name,
+                ["model"] = v => v.Model.Name,
+                ["contactName"] = v => v.ContactName,
+                ["id"] = v => v.Id,
+            };
+
+            if (queryObj.IsSortAscending)
+                query = query.OrderBy(columnsMap[queryObj.SortBy]);
+            else
+                query = query.OrderByDescending(columnsMap[queryObj.SortBy]);
             
             return await query.ToListAsync();
         }
