3c28f2f9c2583253b4ae0abe21885cf0ce1a2e0f
w3ll0n <wellon676@gmail.com>
2017-11-19 22:57:20 +0300

    extend the domain and add photos to vehicle

--- Migrations/20171119195151_AddPhoto.Designer.cs
+++ Migrations/20171119195151_AddPhoto.Designer.cs
@@ -11,9 +11,10 @@ using vega.Persistence;
 namespace vega.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171119195151_AddPhoto")]
+    partial class AddPhoto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -10,6 +10,7 @@ namespace vega.Persistence
         public DbSet<Feature> Features { get; set; }
         public DbSet<Model> Models { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
         {
